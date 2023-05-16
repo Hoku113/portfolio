6e35fb8c5a53dd428b3f965007863bcb1bfe8d41
Hoku113 <hokuto.u0113@gmail.com>
2023-05-17 03:57:41 +0900

    add video files

--- src/component/personal_product.js
+++ src/component/personal_product.js
@@ -1,41 +1,39 @@
-// images
-import preventer from "../assets/preventer.png"
+// media
 import egate from "../assets/egate.png"
+import video from "../videos/preventer.mp4"
 
 // css
 import "../css/personal_product.scss"
+
 
 // personal or team product
 const PersonalProduct = () => {
     return (
         <div>
             <div class="preventer">
-                <div class="preventer-flex">
                     <h3>Preventer</h3>
                     <p>
                         泥酔者が駅のホームから線路に落下することを防止するためのシステム<br/>
                         Intelが提供しているOpenVINOを利用して人間の骨格を表示し、<br/>
                         その人が酔っているのか、酔っていないのかを分析することができます。
                     </p>
-                    <figure class="preventer-img">
-                    <img src={preventer} alt="preventer"/>
-                    </figure>
-                </div>
+                    <video width="480" height="480" controls>
+                        <source src={video} type="video/mp4"/>
+                    </video>
             </div>
 
-            <div class="E-gate">
-                <div class="egate-flex">
-                    <figure class="egate-img">
-                        <img src={egate} alt="egate"/>
-                    </figure>
-                    <h3>E-gate</h3>
-                    <p>
-                        顔認証を利用した出席管理、入退出管理をするためのシステム<br/>
-                        WEBアプリケーションとして公開をすることで、普段使っているスマートフォンやタブレット、<br/>
-                        ノートpcですぐに実行することができます<br/>
-                    </p>
-                </div>
+            <div class="egate">
+                <figure class="egate-img">
+                    <img src={egate} alt="egate"/>
+                </figure>
+                <h3>E-gate</h3>
+                <p>
+                    顔認証を利用した出席管理、入退出管理をするためのシステム<br/>
+                    WEBアプリケーションとして公開をすることで、普段使っているスマートフォンやタブレット、<br/>
+                    ノートpcですぐに実行することができます<br/>
+                </p>
             </div>
+
 
         </div>
 
--- src/component/profile.js
+++ src/component/profile.js
@@ -1,3 +1,5 @@
+import ContuctMe from "./contuctme"
+
 // images
 import myimage from "../assets/myimage.png"
 import azureAI from "../assets/azureAI.png"
@@ -11,12 +13,13 @@ const Profile = () => {
 
     return (
         <div class="profile">
-            <h2>About me</h2>
             <div class="profileflex">
+                <h2>About me</h2>
                     <div class="text">
                         <ul id="name">name: 内田北斗</ul>   
                         <ul id="age">age: 21</ul>
                         <ul id="major">major: Artificial Inteligence</ul>
+                        <ContuctMe/>
                     </div>
                 <figure  class="myimage">
                     <img src={myimage} alt="myimage"/>
